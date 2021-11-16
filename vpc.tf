c3c7b4732517e388a688e5d3b48af2263b5050a9
ANAND AU <anandaulinux@gmail.com>
2021-11-16 20:03:41 +0530

    Inital commit

---
+++ vpc.tf
@@ -0,0 +1,146 @@
+#Creating VPC
+resource "aws_vpc" "default" {
+    cidr_block = var.cidr_block
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags = merge(
+    {
+      Name        = var.name,
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Creating Internet Gateway
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+
+  tags = merge(
+    {
+      Name        = "gwInternet",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Creating Public Routing Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  tags = merge(
+    {
+      Name        = "PublicRouteTable",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Add IGW entry in public routing table
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+#Create public subnet
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    {
+      Name        = "PublicSubnet",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Associate public subnet in public routing table
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Creating EIP for NATGateway
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+# Creating NATGateway
+resource "aws_nat_gateway" "default" {
+  depends_on = [aws_internet_gateway.default]
+  
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(aws_subnet.public.*.id, 0)
+
+  tags = merge(
+    {
+      Name        = "gwNAT",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Create private routing table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.default.id
+
+  tags = merge(
+    {
+      Name        = "PrivateRouteTable",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Attach NATGateway in private routing table
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.default.id
+}
+
+
+#Create private subnet
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = merge(
+    {
+      Name        = "PrivateSubnet",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+#Associate Private subnet to private Routing table
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
